10db8dec1e5653e5898d55f0929d7637da70ee68
rajibulislam60 <rajibulislam60707877@gmail.com>
2024-10-22 21:23:13 +0600

    http server test working'
    clear

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-function data(a,b) {
-    console.log('a+b=', a+b)
-}
+// function data(a,b) {
+//     console.log('a+b=', a+b)
+// }
 
-data(15, 17)
+// data(15, 17)
+
+const http = require('http');
+
+http.createServer(function(req, res){
+    res.write('Hello World! my name is rajibul islam rajib');
+    res.end();
+}).listen(5000)
