3ada8a541ff0cd0d878673e96a654c4b0243aec9
rajibulislam60 <rajibulislam60707877@gmail.com>
2024-10-30 13:47:22 +0600

    nodemon install

--- index.js
+++ index.js
@@ -28,22 +28,24 @@
 // }).listen(5000)
 
 // -----------------url module test-----------------------
-const http = require("http");
-const url = require("url");
-const fs = require("fs");
+// const http = require("http");
+// const url = require("url");
+// const fs = require("fs");
 
-http
-  .createServer(function (req, res) {
-    let q = url.parse(req.url, true);
-    let filename = "." + q.pathname;
-    fs.readFile(filename, function (err, data) {
-      if (err) {
-        res.writeHead(404, { "Content-Type": "text/html" });
-        return res.end("404 Not Found");
-      }
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(data);
-      return res.end();
-    });
-  })
-  .listen(5000);
+// http
+//   .createServer(function (req, res) {
+//     let q = url.parse(req.url, true);
+//     let filename = "." + q.pathname;
+//     fs.readFile(filename, function (err, data) {
+//       if (err) {
+//         res.writeHead(404, { "Content-Type": "text/html" });
+//         return res.end("404 Not Found");
+//       }
+//       res.writeHead(200, { "Content-Type": "text/html" });
+//       res.write(data);
+//       return res.end();
+//     });
+//   })
+//   .listen(5000);
+// ---------------------------postman check------------------------
+
