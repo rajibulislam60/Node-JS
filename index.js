124fe197c41d9cbd86ac02bab7efcaa3b3ffc9d5
rajibulislam60 <rajibulislam60707877@gmail.com>
2024-11-02 21:42:32 +0600

    node js os modules check

--- index.js
+++ index.js
@@ -47,5 +47,14 @@
 //     });
 //   })
 //   .listen(5000);
-// ---------------------------postman check------------------------
-
+// --------------------------- os modules ------------------------
+const os = require("os");
+console.log("Platform: " + os.platform());
+console.log("Architecture: " + os.arch());
+console.log("Release: " + os.release());
+console.log("Type of Computer: " + os.type());
+console.log("Update time: " + os.uptime());
+console.log("Total memory; " + os.totalmem());
+console.log("Free memory; " + os.freemem());
+console.log("UserInfo: " + os.userInfo());
+console.log("Host Name: " + os.hostname());
